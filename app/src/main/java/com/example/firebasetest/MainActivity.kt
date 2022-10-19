5fd22ebfac45e994410fd8097f72ec8dca69fcbf
a21704574 <a21704574@alunos.ulht.pt>
2022-10-19 20:43:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/firebasetest/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.firebasetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null){
+            reload()
+        }
+    }
+
+    private fun reload(){
+        
+    }
+}
